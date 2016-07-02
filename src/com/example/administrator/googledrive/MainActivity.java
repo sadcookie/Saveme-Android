f861aa1aabf97e78d45698db5ffc8aaa2b5909a5
sadcookie <lee10241993@gmail.com>
2016-07-02 15:54:38 +0900

    Google Drive - File List, File Download

--- src/com/example/administrator/googledrive/MainActivity.java
+++ src/com/example/administrator/googledrive/MainActivity.java
@@ -58,10 +58,6 @@ public class MainActivity extends Activity
     private static final String PREF_ACCOUNT_NAME = "accountname";
     private static final String[] SCOPES = { DriveScopes.DRIVE, DriveScopes.DRIVE_METADATA };
 
-    /**
-     * Create the main activity.
-     * @param savedInstanceState previously saved instance data.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,18 +68,11 @@ public class MainActivity extends Activity
                 getApplicationContext(), Arrays.asList(SCOPES))
                 .setBackOff(new ExponentialBackOff());
         getResultsFromApi();
-        mOutputText.setText("구글 드라이브 파일");
+        mOutputText.setText("File of Google Drive");
 
     }
 
 
-    /**
-     * Attempt to call the API, after verifying that all the preconditions are
-     * satisfied. The preconditions are: Google Play Services installed, an
-     * account was selected and the device currently has online access. If any
-     * of the preconditions are not satisfied, the app will prompt the user as
-     * appropriate.
-     */
     private void getResultsFromApi() {
         if (! isGooglePlayServicesAvailable()) {
             acquireGooglePlayServices();
@@ -96,16 +85,6 @@ public class MainActivity extends Activity
         }
     }
 
-    /**
-     * Attempts to set the account used with the API credentials. If an account
-     * name was previously saved it will use that one; otherwise an account
-     * picker dialog will be shown to the user. Note that the setting the
-     * account to use with the credentials object requires the app to have the
-     * GET_ACCOUNTS permission, which is requested here if it is not already
-     * present. The AfterPermissionGranted annotation indicates that this
-     * function will be rerun automatically whenever the GET_ACCOUNTS permission
-     * is granted.
-     */
     @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
     private void chooseAccount() {
         if (EasyPermissions.hasPermissions(
@@ -116,13 +95,11 @@ public class MainActivity extends Activity
                 mCredential.setSelectedAccountName(accountName);
                 getResultsFromApi();
             } else {
-                // Start a dialog from which the user can choose an account
                 startActivityForResult(
                         mCredential.newChooseAccountIntent(),
                         REQUEST_ACCOUNT_PICKER);
             }
         } else {
-            // Request the GET_ACCOUNTS permission via a user dialog
             EasyPermissions.requestPermissions(
                     this,
                     "This app needs to access your Google account (via Contacts).",
@@ -130,17 +107,7 @@ public class MainActivity extends Activity
                     Manifest.permission.GET_ACCOUNTS);
         }
     }
-
-    /**
-     * Called when an activity launched here (specifically, AccountPicker
-     * and authorization) exits, giving you the requestCode you started it with,
-     * the resultCode it returned, and any additional data from it.
-     * @param requestCode code indicating which activity result is incoming.
-     * @param resultCode code indicating the result of the incoming
-     *     activity result.
-     * @param data Intent (containing result data) returned by incoming
-     *     activity result.
-     */
+    
     @Override
     protected void onActivityResult(
             int requestCode, int resultCode, Intent data) {
@@ -179,14 +146,6 @@ public class MainActivity extends Activity
         }
     }
 
-    /**
-     * Respond to requests for permissions at runtime for API 23 and above.
-     * @param requestCode The request code passed in
-     *     requestPermissions(android.app.Activity, String, int, String[])
-     * @param permissions The requested permissions. Never null.
-     * @param grantResults The grant results for the corresponding permissions
-     *     which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
-     */
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -196,34 +155,16 @@ public class MainActivity extends Activity
                 requestCode, permissions, grantResults, this);
     }
 
-    /**
-     * Callback for when a permission is granted using the EasyPermissions
-     * library.
-     * @param requestCode The request code associated with the requested
-     *         permission
-     * @param list The requested permission list. Never null.
-     */
     @Override
     public void onPermissionsGranted(int requestCode, List<String> list) {
         // Do nothing.
     }
 
-    /**
-     * Callback for when a permission is denied using the EasyPermissions
-     * library.
-     * @param requestCode The request code associated with the requested
-     *         permission
-     * @param list The requested permission list. Never null.
-     */
     @Override
     public void onPermissionsDenied(int requestCode, List<String> list) {
         // Do nothing.
     }
 
-    /**
-     * Checks whether the device currently has a network connection.
-     * @return true if the device has a network connection, false otherwise.
-     */
     private boolean isDeviceOnline() {
         ConnectivityManager connMgr =
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -231,11 +172,6 @@ public class MainActivity extends Activity
         return (networkInfo != null && networkInfo.isConnected());
     }
 
-    /**
-     * Check that Google Play services APK is installed and up to date.
-     * @return true if Google Play Services is available and up to
-     *     date on this device; false otherwise.
-     */
     private boolean isGooglePlayServicesAvailable() {
         GoogleApiAvailability apiAvailability =
                 GoogleApiAvailability.getInstance();
@@ -244,10 +180,6 @@ public class MainActivity extends Activity
         return connectionStatusCode == ConnectionResult.SUCCESS;
     }
 
-    /**
-     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
-     * Play Services installation via a user dialog, if possible.
-     */
     private void acquireGooglePlayServices() {
         GoogleApiAvailability apiAvailability =
                 GoogleApiAvailability.getInstance();
@@ -259,12 +191,6 @@ public class MainActivity extends Activity
     }
 
 
-    /**
-     * Display an error dialog showing that Google Play Services is missing
-     * or out of date.
-     * @param connectionStatusCode code describing the presence (or lack of)
-     *     Google Play Services on this device.
-     */
     void showGooglePlayServicesAvailabilityErrorDialog(
             final int connectionStatusCode) {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
@@ -275,11 +201,6 @@ public class MainActivity extends Activity
         dialog.show();
     }
 
-
-    /**
-     * An asynchronous task that handles the Drive API call.
-     * Placing the API calls in their own task ensures the UI stays responsive.
-     */
     private class MakeRequestTask extends AsyncTask<Void, Void, ListViewAdapter>
     implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener{
         private com.google.api.services.drive.Drive mService = null;
@@ -294,10 +215,6 @@ public class MainActivity extends Activity
                     .build();
         }
 
-        /**
-         * Background task to call Drive API.
-         * @param params no parameters needed for this task.
-         */
         @Override
         protected ListViewAdapter doInBackground(Void... params) {
             try {
@@ -309,12 +226,6 @@ public class MainActivity extends Activity
             }
         }
 
-        /**
-         * Fetch a list of up to 10 file names and IDs.
-         * @return List of Strings describing files, or an empty list if no files
-         *         found.
-         * @throws IOException
-         */
         private ListViewAdapter getDataFromApi() throws IOException {
             ListViewAdapter lva = new ListViewAdapter();
             // Get a list of up to 10 files.
@@ -350,24 +261,11 @@ public class MainActivity extends Activity
             }
         }
 
-        /**
-         * 리스트에서 파일을 터치 했을 때 발생하는 메소드
-         * 터치하면 파일의 위치 값을 받아서 파일 이름을 토스트로 출력
-         */
-
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             ListViewItem lvt = (ListViewItem) parent.getItemAtPosition(position);
             Toast.makeText(getBaseContext(),"Selected File = "+lvt.getFileName(),Toast.LENGTH_SHORT).show();
         }
 
-        /**
-         * 길게 터치하면 파일 Id를 받아와서 executeMediaAndDownloadTo 메소드 실행
-         * @param parent
-         * @param view
-         * @param position
-         * @param id
-         * @return
-         */
         public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
             ListViewItem lvt = (ListViewItem) parent.getItemAtPosition(position);
             String FileId= lvt.getFileId();
