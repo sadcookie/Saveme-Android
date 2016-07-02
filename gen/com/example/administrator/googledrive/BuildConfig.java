72223e83ce8f030b109ffc75181af9b6802d3352
sadcookie <lee10241993@gmail.com>
2016-07-02 15:45:53 +0900

    구글 드라이브 리스트, 파일 다운로드

--- gen/com/example/administrator/googledrive/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.administrator.googledrive;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
